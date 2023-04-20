ea3fc456bb2f7013140ea36700c5f2240da37ab1
Vladyslav Lopatin <vladyslav.lopatin@wskz.pl>
2023-04-20 12:36:57 +0200

    fix alert messsage

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,5 +1,5 @@
 @if (session()->has('error'))
-    <x-laravel-alert::banner style="danger" :message="session()" />
+    <x-laravel-alert::banner style="danger" :message="session('error')" />
 @elseif (session()->has('flash.banner'))
     <x-laravel-alert::banner :style="session('flash.bannerStyle', 'success')" :message="__('laravel-alert::alert.' . session('flash.banner'), session('flash.bannerParams', []))" />
 @elseif (count($errors) > 0)
