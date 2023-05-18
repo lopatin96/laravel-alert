ad851fa53685f638b02e1c1c9678f8df68324272
Vladyslav Lopatin <vladyslav.lopatin@wskz.pl>
2023-05-18 12:56:53 +0200

    removed truncate

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -25,7 +25,7 @@
                     </svg>
                 </span>
 
-                <p class="ml-3 font-medium text-sm text-white truncate" x-html="message"></p>
+                <p class="ml-3 font-medium text-sm text-white" x-html="message"></p>
             </div>
 
             <div class="shrink-0 sm:ml-3">
